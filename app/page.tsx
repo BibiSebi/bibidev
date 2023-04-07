93ea87702ffc1f754c9bddf164393061b6f28207
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-04-07 14:07:37 +0200

    feat: added stylings - added unmet peer dep

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,18 @@ export default function Page() {
             }
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome to my personal website
+            <h1 className="text-8xl font-bold tracking-tight text-gray-900">
+              BibiSebi
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Join me in my journey as a Fullstack Developer. This website
+              Hello my name is <b>Bibiana</b> and I am currently working @ Storyblok.
+              Join me in my journey as a Fullstack Engineer. This website
               should be a collection of lessons learned in my professional as
               well as my private life.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div className="mt-4 flex items-center justify-center gap-x-6">
               <a
                 href="#"
                 className="text-base font-semibold leading-7 text-gray-900"
@@ -30,11 +31,7 @@ export default function Page() {
               </a>
             </div>
           </div>
-          <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-            <BackgroundGradient
-              className={'left-[calc(50%+3rem)] sm:left-[calc(50%+36rem)]'}
-            />
-          </div>
+
         </div>
       </div>
     </main>
