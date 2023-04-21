9dedcb6ea5c19869cbd382969798bc271bb206ea
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-04-21 13:54:38 +0200

    feat: placeholder for about page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <div className='relative flex-1  px-6 lg:px-8'>
         <div className='mx-auto max-w-2xl py-32 sm:py-48'>
           <div className='text-center'>
-            <h1 className='text-6xl font-bold tracking-tight text-gray-900 sm:text-8xl'>
+            <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-8xl'>
               🎊 Welcome 🎊
             </h1>
             <p className='mt-6 text-lg leading-8 text-gray-600'>
